2464a53bb0b548a3e19a978828720b249f9fa413
Daniel González Cerviño <daniel.gonzalez@freelancemadrid.es>
2013-12-03 15:29:06 +0100

    created interfaces

---
+++ src/FastFeed/Sanitizer/SanitizerInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the FastFeed package.
+ *
+ * (c) Daniel González <daniel@desarrolla2.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace FastFeed\Sanitizer;
+
+/**
+ * SanitizerInterface
+ */
+class SanitizerInterface {
+
+} 
