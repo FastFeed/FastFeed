61aa4233c596fbda84e21836ff7fc5d38c47cbff
Daniel González Cerviño <daniel.gonzalez@freelancemadrid.es>
2013-12-03 15:21:16 +0100

    initial commit

---
+++ src/FastFeed/FastFeed.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the FastFeed package.
+ *
+ * (c) Daniel González <git@github.com:FastFeed/FastFeed.git>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * FastFeed
+ */
+class FastFeed {
+
+} 
